5a9e4c17bafa95dcc18a3c811a6ed4984d67fc12
Bishal Singh <bishal8.2k16@gmail.com>
2021-09-27 12:38:06 +0530

    382. Linked List Random Node

---
+++ september/382. Linked List Random Node.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    private ListNode head;
+    Random random;
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    public Solution(ListNode head) {
+        this.head = head;
+        random = new Random();
+    }
+    
+    /** Returns a random node's value. */
+    public int getRandom() {
+        int curVal = head.val;
+        int cur = 2;
+        ListNode temp = head.next;
+        
+        while(temp != null) {
+            int rand = random.nextInt(cur);
+            if(rand == cur-1)curVal = temp.val;
+            temp = temp.next;
+            cur += 1;
+        }
+        return curVal;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
